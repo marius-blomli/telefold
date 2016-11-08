0dd7e6cd986e2177938111e800600121cebaa006
Marius Blomli <marius.blomli@bouvet.no>
2016-11-08 17:25:04 +0100

    Configuration

--- kernel_density/kernel_density_config.py
+++ kernel_density/kernel_density_config.py
@@ -1,30 +1,30 @@
 config = {
 	'matrikkel_zip_files': [{
-		'zip_name': 'C:/Users/marius.blomli/Documents/NTNU-Prosjekt/Larvik/Matrikkeldata/SOSIuttrekk/07_Vestfold/0709_Larvik/UTM32_Euref89/Shape/32_Matrikkeldata_0709.zip',
+		'zip_name': 'Y:/kartdata/Matrikkeldata/SOSIuttrekk/07_Vestfold/0709_Larvik/UTM32_Euref89/Shape/32_Matrikkeldata_0709.zip',
 		'target_shape_prefix': '32_0709adresse_punkt'
 	},{
-		'zip_name': 'C:/Users/marius.blomli/Documents/NTNU-Prosjekt/Sandefjord/Matrikkeldata/SOSIuttrekk/07_Vestfold/0706_Sandefjord/UTM32_Euref89/Shape/32_Matrikkeldata_0706.zip',
+		'zip_name': 'Y:/kartdata/Matrikkeldata/SOSIuttrekk/07_Vestfold/0706_Sandefjord/UTM32_Euref89/Shape/32_Matrikkeldata_0706.zip',
 		'target_shape_prefix': '32_0706adresse_punkt'
 	},{
-		'zip_name': 'C:/Users/marius.blomli/Documents/NTNU-Prosjekt/Andebu/Matrikkeldata/SOSIuttrekk/07_Vestfold/0719_Andebu/UTM32_Euref89/Shape/32_Matrikkeldata_0719.zip',
+		'zip_name': 'Y:/kartdata/Matrikkeldata/SOSIuttrekk/07_Vestfold/0719_Andebu/UTM32_Euref89/Shape/32_Matrikkeldata_0719.zip',
 		'target_shape_prefix': '32_0719adresse_punkt'
 	},{
-		'zip_name': 'C:/Users/marius.blomli/Documents/NTNU-Prosjekt/Bamble/Matrikkeldata/SOSIuttrekk/08_Telemark/0814_Bamble/UTM32_Euref89/Shape/32_Matrikkeldata_0814.zip',
+		'zip_name': 'Y:/kartdata/Matrikkeldata/SOSIuttrekk/08_Telemark/0814_Bamble/UTM32_Euref89/Shape/32_Matrikkeldata_0814.zip',
 		'target_shape_prefix': '32_0814adresse_punkt'
 	},{
-		'zip_name': 'C:/Users/marius.blomli/Documents/NTNU-Prosjekt/Notteroy/Matrikkeldata/SOSIuttrekk/07_Vestfold/0722_Notteroy/UTM32_Euref89/Shape/32_Matrikkeldata_0722.zip',
+		'zip_name': 'Y:/kartdata/Matrikkeldata/SOSIuttrekk/07_Vestfold/0722_Notteroy/UTM32_Euref89/Shape/32_Matrikkeldata_0722.zip',
 		'target_shape_prefix': '32_0722adresse_punkt'
 	},{
-		'zip_name': 'C:/Users/marius.blomli/Documents/NTNU-Prosjekt/Porsgrunn/Matrikkeldata/SOSIuttrekk/08_Telemark/0805_Porsgrunn/UTM32_Euref89/Shape/32_Matrikkeldata_0805.zip',
+		'zip_name': 'Y:/kartdata/Matrikkeldata/SOSIuttrekk/08_Telemark/0805_Porsgrunn/UTM32_Euref89/Shape/32_Matrikkeldata_0805.zip',
 		'target_shape_prefix': '32_0805adresse_punkt'
 	},{
-		'zip_name': 'C:/Users/marius.blomli/Documents/NTNU-Prosjekt/Skien/Matrikkeldata/SOSIuttrekk/08_Telemark/0806_Skien/UTM32_Euref89/Shape/32_Matrikkeldata_0806.zip',
+		'zip_name': 'Y:/kartdata/Matrikkeldata/SOSIuttrekk/08_Telemark/0806_Skien/UTM32_Euref89/Shape/32_Matrikkeldata_0806.zip',
 		'target_shape_prefix': '32_0806adresse_punkt'
 	},{
-		'zip_name': 'C:/Users/marius.blomli/Documents/NTNU-Prosjekt/Stokke/Matrikkeldata/SOSIuttrekk/07_Vestfold/0720_Stokke/UTM32_Euref89/Shape/32_Matrikkeldata_0720.zip',
+		'zip_name': 'Y:/kartdata/Matrikkeldata/SOSIuttrekk/07_Vestfold/0720_Stokke/UTM32_Euref89/Shape/32_Matrikkeldata_0720.zip',
 		'target_shape_prefix': '32_0720adresse_punkt'
 	},{
-		'zip_name': 'C:/Users/marius.blomli/Documents/NTNU-Prosjekt/Tjome/Matrikkeldata/SOSIuttrekk/07_Vestfold/0723_Tjome/UTM32_Euref89/Shape/32_Matrikkeldata_0723.zip',
+		'zip_name': 'Y:/kartdata/Matrikkeldata/SOSIuttrekk/07_Vestfold/0723_Tjome/UTM32_Euref89/Shape/32_Matrikkeldata_0723.zip',
 		'target_shape_prefix': '32_0723adresse_punkt'
 	}],
 	'temp_directory': 'C:/temp/ntnu_temp/',
@@ -32,7 +32,7 @@ config = {
 	'file_name_raw_kernel_density_temp': 'kernel_density_raw.img',
 	'area_rectangle': '6550000 550000 6560000 560000',
 	'area_rectangle_x': '6533900 532700 6561800 590000',
-	'file_name_kernel_density': 'C:/temp/ntnu_data/kernel_density_1000_5000.img',
-	'kernel_density_cell_size': 1000,
+	'file_name_kernel_density': 'Y:/prosesserte_data/kernel_density_25_5000.img',
+	'kernel_density_cell_size': 25,
 	'kernel_density_search_radius': 5000
 }
